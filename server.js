6e183d2b20a29798a22c5e4476bb57564ec4a6d4
Wouter Kodde <wouterk99@gmail.com>
2018-04-25 14:35:54 +0200

    Added /random

--- server.js
+++ server.js
@@ -4,5 +4,15 @@ var app = express()
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
+
+app.get('/random', function (req, res) {
+
+  let list = ["bye", "cya", "challas", "doei", "daag", "de meur", "trusten", "goodnight!"];
+  let random = list[Math.floor(Math.random() * list.length)];
+  res.status(200);
+  res.json({
+    message: random
+  })
+})
  
 app.listen(3000)
